7f7dc2da474d71501dae5e385b6d69a853b2fde9
caopham123 <110911665+caopham123@users.noreply.github.com>
2022-09-13 22:18:30 +0700

    Add files via upload

---
+++ bangCuuChuong.py
@@ -0,0 +1,6 @@
+####    BANG CUU CHUONG
+for n in range (2,10,1):
+    print('BANG CUU CHUONG',n)
+    for i in range (1,11,1):
+        rs = n*i
+        print('{n} x {i} = {rs}'.format(n=n,i=i,rs=rs))
